849562b1337f6d2b9b8b1ced2dbe224b60d88040
Anastasya Katrushenko <Anastasya.2701@icloud.com>
2024-07-20 21:54:18 +0300

    workready

--- homework6_1.py
+++ homework6_1.py
@@ -1,56 +1,54 @@
 from collections import UserDict
 
 class Field: #Базовий клас для полів запису 
-    def __init__(self, value):
-        self.value = value
+       def __init__(self, value):
+            self.value = value
 
-    def __str__(self):
-        return str(self.value)
+       def __str__(self):
+            return str(self.value)
 
 class Name(Field):   #Клас для зберігання імені контакту. Обов'язкове поле.
-    def __init__(self, value):
-           self.value = value
-           super().__init__(value)		              
+       def __init__(self, value):
+            self.value = value
+            super().__init__(value)		              
 
 class Phone(Field): #Клас для зберігання номеру телефона. Має валідацію формату.
-		def __init__(self, phone_number):
-                        if len(phone_number) != 10: # Лише 10 цифр
-                               raise ValueError("Номер телефону має складатись з 10 цифр.") # В іншому випадку помилка
-                        super().__init__(phone_number)
+	def __init__(self, phone_number:str):
+              if not phone_number.isdigit() or len(phone_number) != 10: # Лише 10 цифр
+                      raise ValueError("Номер телефону має складатись з 10 цифр.") # В іншому випадку помилка
+              super().__init__(phone_number)
        
 class Record: # Клас для зберігання інформації про контакт (ім'я та номер телефону).
-    def __init__(self, name):
-        self.name = Name(name)
-        self.phones = []
+       def __init__(self, name: str):
+            self.name = Name(name)
+            self.phones = []
 
-    def add_phone(self, phone): # додавання
-           for phone in self.phones:
-                  if phone.value == phone:
-                         return
-                  self.phone.append(Phone(phone))
+       def add_phone(self, phone: str) -> None: # додавання
+              self.phones.append(Phone(phone))
 
-    def remove_phone(self, phone): # видалення
-           for phone in self.phones:
-                  if phone.value == phone:
-                         self.phones.remove(phone)
-                         return 
-                  raise ValueError
+       def remove_phone(self, phone: str) -> None: # видалення
+              for number in self.phones:
+                     if number.value == phone:
+                          self.phones.remove(number)
+                          return 
+                     raise ValueError
            
-    def edit_phone(self, old_phone, new_phone): # зміна
-           for phone in self.phones:
-                  if phone.value == old_phone:
-                         phone.value = new_phone
-                         return
-                  raise ValueError
+       def edit_phone(self, phone:str, new_phone:str) -> None: # зміна
+              for number in self.phones:
+                     if number.value == phone:
+                           number.value = new_phone
+                           return
+                     raise ValueError("Номер телефону не знайдено.")
            
-    def find_phone(self, phone): # пошук
-           for phone in self.phones:
-                  if phone.value == phone:
+       def find_phone(self, phone:str) -> None: # пошук
+              for number in self.phones:
+                     if number.value == phone:
                          return phone
-                  raise ValueError
+                     else:
+                         raise ValueError("Номер телефону не знайдено.")
 
-    def __str__(self):
-        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+       def __str__(self):
+           return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
 
 class AddressBook(UserDict): # Клас для зберігання всіх контатів
        def __init__(self):
