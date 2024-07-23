c71294f0c3e3140c2cedd6462669ac13a6bcb833
Anastasya Katrushenko <Anastasya.2701@icloud.com>
2024-07-23 14:36:42 +0300

    homework ready

--- homework6_1.py
+++ homework6_1.py
@@ -26,43 +26,41 @@ class Record: # Клас для зберігання інформації про
        def add_phone(self, phone: str) -> None: # додавання
               self.phones.append(Phone(phone))
 
-       def remove_phone(self, phone: str) -> None: # видалення
+       def remove_phone(self, phone): # видалення
               for number in self.phones:
                      if number.value == phone:
                           self.phones.remove(number)
                           return 
-                     raise ValueError
+              raise ValueError
            
        def edit_phone(self, phone:str, new_phone:str) -> None: # зміна
               for number in self.phones:
                      if number.value == phone:
                            number.value = new_phone
                            return
-                     raise ValueError("Номер телефону не знайдено.")
+              raise ValueError("Номер телефону не знайдено.")
            
-       def find_phone(self, phone:str) -> None: # пошук
+       def find_phone(self, phone): # пошук
               for number in self.phones:
                      if number.value == phone:
                          return phone
-                     else:
-                         raise ValueError("Номер телефону не знайдено.")
+              raise ValueError("Номер телефону не знайдено.")
 
        def __str__(self):
            return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
 
 class AddressBook(UserDict): # Клас для зберігання всіх контатів
-       def __init__(self):
-              self.data = {}
-              super().__init__()
 
-       def add_record(self, contact): # Метод додавання контакту
-              self.data[contact.name.value] = contact
+       def add_record(self, record: Record): # Метод додавання контакту
+              name = record.name.value
+              self.data.update({name: record})
 
-       def find(self,contact): # Метод для знаходження та перевірки контакту
-              return self.data.get(contact)
+       def find(self,name): # Метод для знаходження та перевірки контакту
+              return self.get(name)
        
-       def delete(self, contact): # Метод для видалення контакту
-              del self.data[contact.name.value]
+       def delete(self,name): # Метод для видалення контакту
+              del self[name]
 
        def __str__(self):
               return "\n".join(str(record) for record in self.data.values())
+       
